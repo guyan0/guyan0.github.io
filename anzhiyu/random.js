0b07316449d2f45ea113b308c58a3f42963c010b
guyan0 <2521671732@qq.com>
2024-12-28 00:03:45 +0800

    Site updated: 2024-12-28 00:03:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/27/hello-world/"];function toRandomPost(){
+var posts=["2024/12/27/hello-world/","2024/12/28/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
